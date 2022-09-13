6dba0e3104c6e607413d8efac0df76b6962739c8
Kessy Jhony 1º info <112711350+Kessy0875@users.noreply.github.com>
2022-09-13 09:26:32 -0300

    Add files via upload

---
+++ Atividade 01.py
@@ -0,0 +1,5 @@
+pes = float ( input ("Informe a medida em pes: ") )
+polegadas = pes * 12
+jardas = pes / 3
+milhas = jardas / 1760
+print ( f"Sua medida em POLEGADAS: { round ( polegadas,2 ) }, em JARDAS: { round ( jardas,2 ) }, e em MILHAS: { round ( milhas,2 ) } ")
