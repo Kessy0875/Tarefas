112627527e35f3f071bc5c5fde53c24c57ab7435
Kessy Jhony 1º info <112711350+Kessy0875@users.noreply.github.com>
2022-09-19 12:47:43 -0300

    Add files via upload

---
+++ Atividade 19.04.2022.py
@@ -0,0 +1,12 @@
+
+media= float(input("Escreva sua média: "))
+if media >= 10:
+    print("PARABÉNS ( ͡▀ ̿ ͜ʖ ͡▀ ̿)👌")
+elif media >= 7:
+    print("Okay ( ͡❛ ᴗ ͡❛)✌")
+elif media >= 5:
+    print("Recuperação (ㆆ_ㆆ)")
+elif media >= 4:
+    print("Vish KK (⊙.⊙(☉̃ₒ☉)⊙.⊙)")
+else:
+    print("SE LASCOU ☠ (╥︣﹏᷅╥) ☠")
