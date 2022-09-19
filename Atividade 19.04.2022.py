2065363538e9c92a2062b3eb3ba2f9042f9632d4
Kessy Jhony 1º info <112711350+Kessy0875@users.noreply.github.com>
2022-09-19 12:35:20 -0300

    Add files via upload

---
+++ Atividade 19.04.2022.py
@@ -0,0 +1,12 @@
+
+media= float(input("Escreva sua média: "))
+if media >= 10:
+    print("( ͡▀ ̿ ͜ʖ ͡▀ ̿)👌")
+elif media >= 7:
+    print("Aprovado")
+elif media >= 5:
+    print("Recuperação")
+elif media >= 4:
+    print("Critico")
+else:
+    print("Reprovado")
